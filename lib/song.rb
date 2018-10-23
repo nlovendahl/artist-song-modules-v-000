6854409ec3d20cbf9df2405dea19ee569b48729b
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-23 21:29:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -21,7 +22,4 @@ class Song
     @artist = artist
   end
 
-  #def to_param
-  #  name.downcase.gsub(' ', '-')
-  #end
 end
